438efbcc05909035ab7fe350da4ece6b6d223160
JLizama25 <Jaque.lidea1@gmail.com>
2025-11-13 14:08:31 -0600

    Actualizado script start para usar app.js

--- app.js
+++ app.js
@@ -31,4 +31,6 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/estudiantes', require('./routes/estudiantesRoutes'));
  
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
